940c4ba12f3299caa1b315be6680ac2f33ae81b6
rajathnup <101017691+rajathnup@users.noreply.github.com>
2022-03-12 05:43:38 +0530

    Update subsetsum_recursionapproach.cpp

--- DAY4/subsetsum_recursionapproach.cpp
+++ DAY4/subsetsum_recursionapproach.cpp
@@ -1,1 +1,30 @@
+//problem statement: https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
 
+
+class Solution{   
+public:
+    bool isSubsetSum(int N, int arr[], int sum){
+        // code here
+        if(N==0)
+        {
+            return (sum==0)?true:false;
+        }
+        
+        if(issafe(sum)==true)
+        {
+            return isSubsetSum(N-1,arr,sum)+isSubsetSum(N-1,arr,sum-arr[N-1]);
+        }
+    }
+    
+    bool issafe(int sum)
+    {
+        if(sum<0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
